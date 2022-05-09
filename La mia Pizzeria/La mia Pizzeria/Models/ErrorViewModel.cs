bb807e4ebdf89e70515f4b4a74a4e5663f581747
Gennaro <gennarinodi02@gmail.com>
2022-05-09 14:08:48 +0200

    Add project files.

---
+++ La mia Pizzeria/La mia Pizzeria/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace La_mia_Pizzeria.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
